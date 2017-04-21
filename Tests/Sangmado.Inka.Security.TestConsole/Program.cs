565ef5a9055980fed498701c31f51eb479fb2905
gaochundong <gaochundong@hotmail.com>
2017-04-21 23:16:35 +0800

    test interchange with bc with private and public keys

--- Tests/Sangmado.Inka.Security.TestConsole/Program.cs
+++ Tests/Sangmado.Inka.Security.TestConsole/Program.cs
@@ -24,10 +24,15 @@ namespace Sangmado.Inka.Security.TestConsole
             var privateKeyXmlString = DotNetUtilities.ToRSA(privateKey).ToXmlString(true);
             var publicKeyXmlString = DotNetUtilities.ToRSA(publicKey).ToXmlString(false);
 
-            Test_SHA1withRSA_Sign_Verify(privateKeyXmlString, publicKeyXmlString);
+            var verified1 = Test_SHA1withRSA_Sign_Verify(privateKeyXmlString, publicKeyXmlString);
+            var verified2 = Test_SHA1withRSA_Sign_With_BouncyCastle_Then_Verify_With_DotNet(privateKey, publicKeyXmlString);
+            var verified3 = Test_SHA1withRSA_Sign_With_DotNet_Then_Verify_With_BouncyCastle(privateKeyXmlString, publicKey);
+
+            Console.WriteLine("{0}, {1}, {2}", verified1, verified2, verified3);
         }
 
-        public static void Test_SHA1withRSA_Sign_Verify(string privateKeyXmlString, string publicKeyXmlString)
+        public static bool Test_SHA1withRSA_Sign_Verify(
+            string privateKeyXmlString, string publicKeyXmlString)
         {
             string content = @"Hello World";
             byte[] buffer = Encoding.UTF8.GetBytes(content);
@@ -35,7 +40,43 @@ namespace Sangmado.Inka.Security.TestConsole
             string signature = SHA1withRSA.Sign(privateKeyXmlString, buffer);
             bool verified = SHA1withRSA.Verify(publicKeyXmlString, buffer, signature);
 
-            Console.WriteLine(verified);
+            return verified;
+        }
+
+        public static bool Test_SHA1withRSA_Sign_With_BouncyCastle_Then_Verify_With_DotNet(
+            RsaPrivateCrtKeyParameters rsaPrivate, string publicKeyXmlString)
+        {
+            string content = @"Hello World";
+            byte[] buffer = Encoding.UTF8.GetBytes(content);
+
+            var signer = SignerUtilities.GetSigner(@"SHA-1withRSA");
+            signer.Init(true, rsaPrivate);
+            signer.BlockUpdate(buffer, 0, buffer.Length);
+            byte[] signedHash = signer.GenerateSignature();
+
+            string signature = Convert.ToBase64String(signedHash);
+
+            bool verified = SHA1withRSA.Verify(publicKeyXmlString, buffer, signature);
+
+            return verified;
+        }
+
+        public static bool Test_SHA1withRSA_Sign_With_DotNet_Then_Verify_With_BouncyCastle(
+            string privateKeyXmlString, RsaKeyParameters rsaPublic)
+        {
+            string content = @"Hello World";
+            byte[] buffer = Encoding.UTF8.GetBytes(content);
+
+            string signature = SHA1withRSA.Sign(privateKeyXmlString, buffer);
+
+            var signer = SignerUtilities.GetSigner(@"SHA-1withRSA");
+            signer.Init(false, rsaPublic);
+            signer.BlockUpdate(buffer, 0, buffer.Length);
+
+            byte[] signedHash = Convert.FromBase64String(signature);
+            bool verified = signer.VerifySignature(signedHash);
+
+            return verified;
         }
     }
 }
